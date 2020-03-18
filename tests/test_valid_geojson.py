2c68aba9a1c5ad969140e0d0c7207cbe85afa458
Bill Fanselow <bfanselow@gmail.com>
2020-03-18 02:13:43 +0000

    Added point-in-polygon stuff

--- tests/test_valid_geojson.py
+++ tests/test_valid_geojson.py
@@ -6,14 +6,19 @@
 """
 import json
 import pytest
-from app.polygon_geometry import check_polygon_intersection, get_overlap_area
+from app.polygon_geometry import check_polygon_intersection, get_overlap_area, check_point_in_polygon
+
 
 POLY_1 = '{"type": "Polygon", "coordinates": [[[ 100.0, 0.0 ], [ 101.0, 0.0 ], [ 101.0, 1.0 ], [ 100.0, 1.0 ], [ 100.0, 0.0 ]]]}'
 POLY_2 = '{ "type": "Polygon", "coordinates": [[[1208064, 624154], [1208064, 601260], [1231345, 601260], [1231345, 624154], [1208064, 624154]]] }'
- 
+POLY_3 = '{ "type": "Polygon", "coordinates": [[[24.950899, 60.169158], [24.953492, 60.169158], [24.953510, 60.170104], [24.950958, 60.169990], [24.950899, 60.169158]]] }'
+
+POINT = '{ "type": "Point", "coordinates": [24.952242, 60.1696017] }'
 ##------------------------------------------------------------------------
 INTERSECTION_TRUE  = {'intersects': 1}
 INTERSECTION_FALSE  = {'intersects': 0}
+IS_WITHIN_TRUE  = {'is_within': 1}
+IS_WITHIN_FALSE  = {'is_within': 0}
 ##------------------------------------------------------------------------
 def test_polygon_intersection():
     ## Test check_polygon_intersection() with two polygons that do intersect 
@@ -54,3 +59,15 @@ def test_colorado_montana_overlap_area():
 
       result = check_polygon_intersection(json_colorado, json_montana) 
       assert (result == INTERSECTION_FALSE) == True
+##------------------------------------------------------------------------
+def test_point_in_polygon_true():
+    ## Test point_in_polygon() for True case 
+    result = check_point_in_polygon(point=POINT, polygon=POLY_3) 
+    assert (result == IS_WITHIN_TRUE) == True
+
+##------------------------------------------------------------------------
+def test_point_in_polygon_false():
+    ## Test point_in_polygon() for False case 
+    result = check_point_in_polygon(point=POINT, polygon=POLY_2) 
+    assert (result == IS_WITHIN_FALSE) == True
+
